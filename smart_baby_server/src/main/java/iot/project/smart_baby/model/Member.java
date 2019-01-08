3e73d10a933e6cc7791264c775eecc67f9ba2421
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-08 13:44:59 +0900

    Spring-DB 연동 완료

--- smart_baby_server/src/main/java/iot/project/smart_baby/model/Member.java
+++ smart_baby_server/src/main/java/iot/project/smart_baby/model/Member.java
@@ -2,6 +2,9 @@ package iot.project.smart_baby.model;
 
 import java.util.Date;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,22 +15,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Member {
-//	@NotEmpty(message="필수 항목입니다.")
-//	@Length(min=4, message="4글자 이상 입력하세요.")
+	@NotEmpty(message="필수 항목입니다.")
+	@Length(min=4, message="4글자 이상 입력하세요.")
 	private String userId;
-//	@NotEmpty(message="필수 항목입니다.")
+	@NotEmpty(message="필수 항목입니다.")
 	private String password;
 	
 //	private String salt;		// demo type 개발 완료 후 추가 개발 사항
 	
-//	@NotEmpty(message="필수 항목입니다.")
+	@NotEmpty(message="필수 항목입니다.")
 	private String parName;
 	private String parBirth;
-//	@NotEmpty(message="필수 항목입니다.")
+	@NotEmpty(message="필수 항목입니다.")
 	private String babyName;
-//	@NotEmpty(message="필수 항목입니다.")
+	@NotEmpty(message="필수 항목입니다.")
 	private Date babyBirth;
-//	@NotEmpty(message="필수 항목입니다.")
+	@NotEmpty(message="필수 항목입니다.")
 	private String email;
 	private Date regDate;
 	private Date updateDate;
